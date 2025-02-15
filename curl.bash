984e6eacae6c5a5c3388501e90262589e719de24
santokhan <santokhan1999@gmail.com>
2025-02-15 15:52:44 +0600

    add client url request

--- curl.bash
+++ curl.bash
@@ -5,3 +5,6 @@ curl -X POST "http://135.125.133.105:8060/" \
 curl -X POST "http://localhost:8060/" \
      -H "Content-Type: multipart/form-data" \
      -F "file=@E:\docker-test\ubuntu\app\tmp.zip"
+
+# curl http://135.125.133.105:8060/files/tmp.zip -o tmp.zip
+# curl http://135.125.133.105:8060/files/tmp2.zip -o tmp2.zip
